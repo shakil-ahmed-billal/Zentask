0585a880c9cf98cee59eb1c744b3c4cbc54d639e
shakil-ahmed-billal <itzxhakil@gmail.com>
2026-02-27 06:57:03 +0600

    feat: initialize Next.js configuration with API rewrites for authentication and React Compiler.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,10 +4,12 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   async rewrites() {
+    const backendUrl =
+      process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8000";
     return [
       {
         source: "/api/auth/:path*",
-        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/:path*`,
+        destination: `${backendUrl}/api/auth/:path*`,
       },
     ];
   },
